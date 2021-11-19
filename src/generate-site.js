bc9fa5b8fcd3d7ae0b643019329f9f6dd4ca7f81
jenmlucas <jenmlucas1@gmail.com>
2021-11-18 21:52:25 -0700

    added css

--- src/generate-site.js
+++ src/generate-site.js
@@ -1,90 +1,82 @@
 const managerHtml = (manager) => {
     return `
-    <div class="container"> 
-        <div class="row align-items-start justify-content-between">
-            <div class= "col-md-4">
-              <div class="card" 
-                 <div class="card-header">
-                   <h1>${manager.name}</h1>
-                    <h2>Manager</h2>
-              </div>
+        <div class= "col-md-4 mt-5 mb-4">
+            <div class="card">
+                <div id="cardHeaderManager" class="card-header">
+                   <h1 class="text-center">${manager.name}</h1>
+                    <h2 class="text-center">Manager</h2>
+                </div>
 
-                 <div class='card-body'>
-                      <p>ID: ${manager.id}<p>
+                <div class='card-body'>
+                      <p>ID: ${manager.id}</p>
                       <p>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
                       <p>Office Number: ${manager.officeNumber}</p>
-                 </div>   
-            </div>   
-        </div>
-    </div>
+                </div>
+            </div>      
+        </div>   
+      
     `;
 }
 
 const engineerHtml = (engineer) => {
     return `
-    <div class="container"> 
-        <div class="row align-items-start justify-content-between">
-            <div class= "col-md-4">
-              <div class="card" 
-                 <div class="card-header">
-                   <h1>${engineer.name}</h1>
-                    <h2>Enigneer</h2>
-              </div>
+        <div class= "col-md-4 mt-5 mb-4">
+            <div class="card"> 
+                <div id="cardHeaderEngineer" class="card-header">
+                   <h1 class="text-center">${engineer.name}</h1>
+                   <h2 class="text-center">Enigneer</h2>
+                </div>
 
-                 <div class='card-body'>
-                      <p>ID: ${engineer.id}<p>
-                      <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-                      <p>Github: <a href="https://github.com/${engineer.github}">${engineer.github}></a></p>
-                 </div>   
+                <div class='card-body'>
+                    <p>ID: ${engineer.id}<p>
+                    <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                    <p>Github: <a href="https://github.com/${engineer.github}" target="blank">${engineer.github}</a></p>
+                </div>  
             </div>   
-        </div>
-    </div>
+        </div>   
     `;
 }
 
 const internHtml = (intern) => {
     return `
-    <div class="container"> 
-        <div class="row align-items-start justify-content-between">
-            <div class= "col-md-4">
-              <div class="card" 
-                 <div class="card-header">
-                   <h1>${intern.name}</h1>
-                    <h2>Enigneer</h2>
-              </div>
+        <div class= "col-md-4 mt-5 mb-4">
+            <div class="card">
+                <div id="cardHeaderIntern" class="card-header">
+                   <h1 class="text-center">${intern.name}</h1>
+                   <h2 class="text-center">Enigneer</h2>
+                </div>
 
-                 <div class='card-body'>
+                <div class='card-body'>
                       <p>ID: ${intern.id}<p>
                       <p>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
                       <p>School: ${intern.school}</p>
-                 </div>   
-            </div>   
-        </div>
-    </div>
+                </div> 
+            </div>  
+        </div>   
     `;
 }
 
 const teamHtml = (teamEmployees) => {
-    let htmlEmployeeCards= [];
-    
+    let htmlEmployeeCards = [];
+
     for (let i = 0; i < teamEmployees.length; i++) {
         console.log(teamEmployees[i])
 
-        if(teamEmployees[i].getRole() === "Manager"){
-           const manager = managerHtml(teamEmployees[i]);
-           htmlEmployeeCards.push(manager)
+        if (teamEmployees[i].getRole() === "Manager") {
+            const manager = managerHtml(teamEmployees[i]);
+            htmlEmployeeCards.push(manager)
         }
-        else if(teamEmployees[i].getRole() === "Engineer"){
+        else if (teamEmployees[i].getRole() === "Engineer") {
             const engineer = engineerHtml(teamEmployees[i]);
             htmlEmployeeCards.push(engineer)
         }
-        else if(teamEmployees[i].getRole() === "Intern"){
+        else if (teamEmployees[i].getRole() === "Intern") {
             const intern = internHtml(teamEmployees[i]);
             htmlEmployeeCards.push(intern)
         }
     }
     let htmlEmployee = htmlEmployeeCards.join('');
-   
+
 
     const generateTeamProfile = generateHTML(htmlEmployee);
     return generateTeamProfile;
@@ -99,8 +91,10 @@ const generateHTML = (htmlEmployee) => {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./style.css"/>
 	<title> Team Profile Generator </title>
 </head>
+
 <body>
 
 <header>
@@ -109,9 +103,12 @@ const generateHTML = (htmlEmployee) => {
 
 <main> 
 
-<section> 
-<div class="row">
+<section>
+<div class="container">
+    <div class="row">
+
         ${htmlEmployee}
+    </div>
 </div>
 </section>
 
